1a23d378cce09e0006d5a1d9597dacd1ce5ed2cd
AntonioLuisGlez <algonzalez.ext@catec.aero>
2024-02-16 10:22:29 +0100

    Start of dymanic tf

--- 8-Transform_File/build/catkin_generated/order_packages.py
+++ 8-Transform_File/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/algonzalez/Desktop/Learning_ROS/8-Transform_File/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/algonzalez/Desktop/Learning_ROS/7-Point_Cloud/devel;/home/algonzalez/Desktop/Learning_ROS/6-Markers/devel;/opt/ros/noetic'.split(';') if '/home/algonzalez/Desktop/Learning_ROS/7-Point_Cloud/devel;/home/algonzalez/Desktop/Learning_ROS/6-Markers/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/algonzalez/Desktop/Learning_ROS/8-Dynamic_tf/devel;/home/algonzalez/Desktop/Learning_ROS/6-Markers/devel;/opt/ros/noetic'.split(';') if '/home/algonzalez/Desktop/Learning_ROS/8-Dynamic_tf/devel;/home/algonzalez/Desktop/Learning_ROS/6-Markers/devel;/opt/ros/noetic' != '' else []
